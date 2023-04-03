beced8f1298d7ecb994f024b765446ea2cc8249b
Andres David Solarte Vidal <andresdavidsv@Andress-MacBook-Pro.local>
2023-04-03 13:30:05 -0500

    first commit

---
+++ src/screens/DetailScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+export const DetailScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>DetailScreen</Text>
+    </View>
+  );
+};
