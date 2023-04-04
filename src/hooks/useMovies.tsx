57e070731c5785aa8b8d9ddf430c6e47dc5619af
andresdavidsv <soy@andresdavidsv.com>
2023-04-04 18:24:29 -0500

    feat: add promises all

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,17 +1,47 @@
 import {useEffect, useState} from 'react';
 import movieDB from '../api/movieDB';
-import {Movie, MovieDBNowPlaying} from '../interfaces/movieInterface';
+import {Movie, MovieDBResponse} from '../interfaces/movieInterface';
+
+interface MoviesState {
+  nowPlaying: Movie[];
+  popular: Movie[];
+  topRated: Movie[];
+  upcoming: Movie[];
+}
 
 export const useMovies = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [moviesOnCinema, setMoviesOnCinema] = useState<Movie[]>([]);
+  const [moviesState, setMoviesState] = useState<MoviesState>({
+    nowPlaying: [],
+    popular: [],
+    topRated: [],
+    upcoming: [],
+  });
+
   const getMovies = async () => {
-    const resp = await movieDB.get<MovieDBNowPlaying>('/now_playing');
-    setMoviesOnCinema(resp.data.results);
+    const nowPlayingPromise = movieDB.get<MovieDBResponse>('/now_playing');
+    const popularPromise = movieDB.get<MovieDBResponse>('/popular');
+    const topRatedPromise = movieDB.get<MovieDBResponse>('/top_rated');
+    const upcomingPromise = movieDB.get<MovieDBResponse>('/upcoming');
+
+    const resp = await Promise.all([
+      nowPlayingPromise,
+      popularPromise,
+      topRatedPromise,
+      upcomingPromise,
+    ]);
+
+    setMoviesState({
+      nowPlaying: resp[0].data.results,
+      popular: resp[1].data.results,
+      topRated: resp[2].data.results,
+      upcoming: resp[3].data.results,
+    });
+
     setIsLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
-  return {moviesOnCinema, isLoading};
+  return {isLoading, ...moviesState};
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import {View, ActivityIndicator, Dimensions} from 'react-native';
+import {View, ActivityIndicator, Dimensions, ScrollView} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
 
 import {useMovies} from '../hooks/useMovies';
 import {MoviePoster} from '../components/MoviePoster';
+import {HorizontalSlider} from '../components/HorizontalSlider';
 
 const {width: windowWidth} = Dimensions.get('window');
 
 export const HomeScreen = () => {
-  const {moviesOnCinema, isLoading} = useMovies();
+  const {isLoading, nowPlaying, popular, topRated, upcoming} = useMovies();
   const {top} = useSafeAreaInsets();
 
   if (isLoading) {
@@ -20,15 +21,21 @@ export const HomeScreen = () => {
     );
   }
   return (
-    <View style={{marginTop: top + 20}}>
-      <View style={{height: 440}}>
-        <Carousel
-          data={moviesOnCinema}
-          renderItem={({item}: any) => <MoviePoster movie={item} />}
-          sliderWidth={windowWidth}
-          itemWidth={300}
-        />
+    <ScrollView>
+      <View style={{marginTop: top + 20}}>
+        <View style={{height: 440}}>
+          <Carousel
+            data={nowPlaying}
+            renderItem={({item}: any) => <MoviePoster movie={item} />}
+            sliderWidth={windowWidth}
+            itemWidth={300}
+            inactiveSlideOpacity={0.9}
+          />
+        </View>
+        <HorizontalSlider title="Populars" movies={popular} />
+        <HorizontalSlider title="Top Rated" movies={topRated} />
+        <HorizontalSlider title="Upcoming" movies={upcoming} />
       </View>
-    </View>
+    </ScrollView>
   );
 };
